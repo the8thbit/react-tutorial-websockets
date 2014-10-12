e461ac821d8e3641043d81659d5d5e90de622b8a
Peter Sussman <the8thbit@gmail.com>
2014-10-12 04:47:21 -0500

    switched server from ajax to websockets with socket.io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /**
- * This file provided by Facebook is for non-commercial testing and evaluation purposes only.
- * Facebook reserves all rights not expressly granted.
+ * This file is modified from a file provided by Facebook for non-commercial testing and evaluation purposes only.
+ * Facebook reserves all rights of the upstream file not expressly granted. because of that, I don't know where 
+ * my version stands lol.'
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -10,29 +11,29 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-var comments = JSON.parse(fs.readFileSync('_comments.json'))
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/comments.json', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
+var fs = require('fs'); 
+var path = require('path'); 
+var express = require('express'); 
+var bodyParser = require('body-parser'); 
+ 
+var app = express(); 
+ 
+var comments = JSON.parse(fs.readFileSync('_comments.json')) 
+app.use('/', express.static(path.join(__dirname, 'public'))); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true})); 
+ 
+var server = require('http').Server(app); 
+var io = require('socket.io').listen(server); 
+io.set('log level', 1); 
+ 
+io.on('connection', function(socket) { 
+  socket.emit('load posts', JSON.stringify(comments)); 
+  socket.on('post', function(post){ 
+  comments.push(post); 
+    socket.broadcast.emit('load posts', JSON.stringify(comments)); 
+  }); 
 });
 
-app.post('/comments.json', function(req, res) {
-  comments.push(req.body);
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
-});
-
-app.listen(3000);
-
+server.listen(3000); 
 console.log('Server started: http://localhost:3000/');
